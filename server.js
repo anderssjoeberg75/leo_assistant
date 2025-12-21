4642704e3c7bcf2a0ff80df4d01aed6526599491
anderssjoeberg75 <anderssjoeberg75@gmail.com>
2025-12-21 13:22:17 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -1,80 +1,27 @@
+// server.js
 const express = require('express');
 const http = require('http');
-const { Server } = require('socket.io');
-const motor = require('./motor');
+const socketIo = require('socket.io');
+const os = require('os');
 const { exec } = require('child_process');
-const si = require('systeminformation');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
-
-let failsafeTimer;
-let batteryVoltage = 0;
-let lowBattery = false;
-
-const LOW_BATTERY_VOLTAGE = 10.5; // adjust for your battery pack
-
-function resetFailsafe() {
-  clearTimeout(failsafeTimer);
-  failsafeTimer = setTimeout(() => {
-    motor.stop();
-  }, 1000);
-}
-
-async function updateStats() {
-  const cpu = await si.currentLoad();
-  const mem = await si.mem();
-  return {
-    cpu: cpu.currentLoad.toFixed(1),
-    ram: ((mem.active / mem.total) * 100).toFixed(1)
-  };
-}
+const io = socketIo(server);
 
 app.use(express.static('public'));
 
-app.post('/restart', (req, res) => {
-  res.sendStatus(200);
-  exec('systemctl restart jarvis', () => {});
+io.on('connection', (socket) => {
+  socket.on('reboot', () => exec('sudo reboot'));
+  socket.on('shutdown', () => exec('sudo shutdown now'));
+  socket.on('restartCamera', () => exec('sudo systemctl restart jarvis-camera.service'));
+
+  setInterval(() => {
+    socket.emit('system', {
+      cpu: os.loadavg()[0].toFixed(2),
+      ram: ((1 - os.freemem()/os.totalmem())*100).toFixed(1)
+    });
+  }, 1000);
 });
 
-app.post('/reboot', (req, res) => {
-  res.sendStatus(200);
-  exec('reboot', () => {});
-});
-
-app.post('/shutdown', (req, res) => {
-  res.sendStatus(200);
-  exec('poweroff', () => {});
-});
-
-app.post('/camera-restart', (req, res) => {
-  res.sendStatus(200);
-  exec('systemctl restart jarvis-camera', () => {});
-});
-
-io.on('connection', socket => {
-  resetFailsafe();
-
-  socket.on('drive', data => {
-    if (lowBattery) {
-      motor.stop();
-      return;
-    }
-    resetFailsafe();
-    motor.setTank(data.speed, data.steering);
-  });
-
-  socket.on('getStats', async () => {
-    const stats = await updateStats();
-    socket.emit('stats', stats);
-  });
-
-  socket.on('disconnect', () => {
-    motor.stop();
-  });
-});
-
-server.listen(3000, () => {
-  console.log('Robot control running on port 3000');
-});
+server.listen(3000, () => console.log('Jarvis running on port 3000'));
